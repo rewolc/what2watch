2da2b038a9bff765c4db463fcfb7276d92046670
rewolc <erobel0@mail.ru>
2022-03-09 19:07:04 +0300

    clearing input

--- src/components/filter.component/filter.jsx
+++ src/components/filter.component/filter.jsx
@@ -3,33 +3,34 @@ import { connect, useDispatch, useSelector } from "react-redux";
 import { addTitle } from "../../redux/tytles/tytles.action";
 import "./filter.scss";
 
-
-
-const Filter = ({add}) => {
+const Filter = ({ add }) => {
   const dispatch = useDispatch();
-  const store = useSelector(store => store)
-  const [name, newletter] = useState();
+  const store = useSelector((store) => store);
+  const [name, newletter] = useState('');
   const handleChange = (event) => {
     newletter(event.target.value);
   };
   const handleSubmit = (event) => {
     dispatch({type: 'FETCH', name})
     event.preventDefault();
-    
+    setTimeout(() => newletter(''),500)
   };
   return (
     <div className="filter">
-      {/* <form onSubmit={handleSubmit}> */}
       <form onSubmit={handleSubmit}>
-        <input type="text" onChange={handleChange} />
-        <input type="submit" value="Добавить"  />
+        <input
+          type="text"
+          placeholder="Введите название"
+          onChange={handleChange}
+          value={name}
+        />
+        <input type="submit" value="Добавить" />
       </form>
     </div>
   );
 };
 
 const mapDispatchToProps = (dispatch) => ({
-  
-   add: (item) => dispatch(addTitle(item)),
+  add: (item) => dispatch(addTitle(item)),
 });
-export default connect(null,mapDispatchToProps)(Filter);
+export default connect(null, mapDispatchToProps)(Filter);
